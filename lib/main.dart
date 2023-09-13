d106e0612341b089515ec541cf30dc398e05450a
GuidoAugusta <muhammadguido29@students.amikom.ac.id>
2023-09-13 23:01:30 +0700

    memperbaiki gridview menu rekomendasi buku

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,10 @@
 import 'package:eresource/pages/borrowmenu_page.dart';
 import 'package:eresource/pages/homepage.dart';
 import 'package:eresource/pages/login_page.dart';
+import 'package:eresource/pages/main_page.dart';
 import 'package:eresource/pages/splash_screen.dart';
 import 'package:flutter/material.dart';
+
 void main() {
   runApp(const MyApp());
 }
@@ -17,6 +19,7 @@ class MyApp extends StatelessWidget {
       routes: {
         '/': (context) => const SplashPages(),
         '/login': (context) => const LoginPage(),
+        '/mainpage': (context) => const MainPage(),
         '/homepage': (context) => const HomePage(),
         '/borrowpage': (context) => const BorrowPage(),
       },
---
+++ lib/pages/main_page.dart
@@ -0,0 +1,46 @@
+import 'package:eresource/pages/borrowmenu_page.dart';
+import 'package:eresource/pages/homepage.dart';
+import 'package:flutter/material.dart';
+
+int currentIndex = 0;
+
+List pages = [HomePage(), BorrowPage(), BorrowPage(), BorrowPage()];
+
+class MainPage extends StatefulWidget {
+  const MainPage({super.key});
+
+  @override
+  State<MainPage> createState() => _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  void onTap(int index) {
+    setState(() {
+      currentIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: pages[currentIndex],
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: onTap,
+        currentIndex: currentIndex,
+        selectedItemColor: Color(0xFF614BC3),
+        unselectedItemColor: Colors.grey.withOpacity(1),
+        selectedLabelStyle: TextStyle(fontSize: 12),
+        showUnselectedLabels: true,
+        elevation: 10,
+        iconSize: 30,
+        items: [
+          BottomNavigationBarItem(label: "Home", icon: Icon(Icons.home)),
+          BottomNavigationBarItem(label: "Cari", icon: Icon(Icons.search)),
+          BottomNavigationBarItem(
+              label: "QR-Code", icon: Icon(Icons.qr_code_scanner_rounded)),
+          BottomNavigationBarItem(label: "Profile", icon: Icon(Icons.person)),
+        ],
+      ),
+    );
+  }
+}
