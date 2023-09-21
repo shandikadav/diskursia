3fcd17019a4ebef806c672e22f35d4425f6fd4c4
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2023-09-21 16:12:51 +0700

    fixing appbar active borrow page

--- lib/pages/activeborrow_page.dart
+++ lib/pages/activeborrow_page.dart
@@ -1,3 +1,4 @@
+import 'package:eresource/pages/homepage.dart';
 import 'package:flutter/material.dart';
 
 class ActiveBorrow extends StatelessWidget {
@@ -7,7 +8,26 @@ class ActiveBorrow extends StatelessWidget {
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("Ini Page Peminjaman Aktif"),
+        backgroundColor: Colors.transparent,
+        elevation: 0,
+        leading: IconButton(
+            onPressed: () {
+              Navigator.pop(context,
+                  MaterialPageRoute(builder: (context) => const HomePage()));
+            },
+            icon: Icon(
+              Icons.arrow_back_ios_new,
+              color: Colors.black,
+              size: 20,
+            )),
+        title: Text(
+          "Pinjaman Aktif",
+          style: TextStyle(
+              color: Colors.black,
+              fontSize: 16,
+              fontFamily: 'Inter',
+              fontWeight: FontWeight.w700),
+        ),
       ),
     );
   }
