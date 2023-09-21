9442dd7f175d591d4c7b65420b65364d6c0958b5
shandikadav <shandikadavid.ardiansyah@students.amikom.ac.id>
2023-09-21 16:02:59 +0700

    adding active borrow page

---
+++ lib/pages/activeborrow_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class ActiveBorrow extends StatelessWidget {
+  const ActiveBorrow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Ini Page Peminjaman Aktif"),
+      ),
+    );
+  }
+}
